af8c5cfdf1787779fdedc444f9fe1db850123900
Igor Tamuz <igortamuz@gmail.com>
2022-09-25 19:09:30 -0300

    chore: adjust code for deploy on heroku

--- src/index.js
+++ src/index.js
@@ -7,4 +7,8 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, () => console.log('Listening port 4000'));
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
+
+// app.listen(5000, () => console.log('Listening port 5000'));
