2c1a3e128109fda2e11aecec6c71016eac0b025d
Igor Tamuz <igortamuz@gmail.com>
2022-09-25 19:54:41 -0300

    Done 2.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import router from "./routers/routers.js"
+import router from "./routers/routers.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server running on port " + process.env.PORT);
 });
 
